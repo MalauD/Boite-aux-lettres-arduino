04b2c21914307b506bf39efdbecba67fa4d8ad47
Malaury <malaurydutour@outlook.fr>
2019-01-27 15:17:12 +0100

    Refacto Recepteur

--- recepteur.ino
+++ recepteur.ino
@@ -1,37 +1,47 @@
 // ==== Recepteur 433 MHz ===============================
 
-#include <VirtualWire.h> 
- 
-uint8_t buf[VW_MAX_MESSAGE_LEN]; 
-uint8_t buflen = VW_MAX_MESSAGE_LEN; 
+#include <VirtualWire.h>
+
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t buflen = VW_MAX_MESSAGE_LEN;
 int switchstate = 0;
-void setup() 
+
+void setup()
 {
-  pinMode(3, INPUT);
-    Serial.begin(9600); 
-    Serial.println("Tuto VirtualWire");     
-    pinMode(2, OUTPUT);
- 
-    vw_setup(2000);
-    vw_rx_start();  
+    SetupIO();
+    SetupCom();
 }
- 
+
+void SetupIO(){
+  pinMode(3, INPUT);
+  pinMode(2, OUTPUT);
+}
+
+void SetupCom(){
+  //Init des comunications avec le serial & le virtual VirtualWire
+  Serial.begin(9600);
+  Serial.println("Recepteur - 433mhz");
+
+  vw_setup(2000);
+  vw_rx_start();
+}
+
 void loop()
 {
    switchstate = digitalRead(3);
    if (switchstate == HIGH) {
-    digitalWrite(2, LOW);
+      digitalWrite(2, LOW);
    }
-    if (vw_wait_rx_max(200)) 
+   if (PresenceOfLetter()){
+      Serial.println("Lettre !");
+      digitalWrite(2, HIGH);
+   }
+}
 
-        if (vw_get_message(buf, &buflen)) 
-        {
-            Serial.print("RX : ");
-            for (byte i = 0; i < buflen; i++) 
-             //Serial.print(buf[i]);   
-             {Serial.print(buf[i]- '0');   
-             }
-             digitalWrite(2, HIGH);
-        }
-    }
+bool PresenceOfLetter(){
+  //Verifie la presence d'un message dans le buffer de VirtualWire puis copie sur le buffer local
+  if(vw_wait_rx_max(200) && vw_get_message(buf, &buflen)){
+    return true;
+  } 
+  return false;
 }
